e68f8c0127b3f4f4b9eac6f8e06014935a6c873c
David Ronnenberg <david.ronnenberg@iml.fraunhofer.de>
2021-07-01 11:43:53 +0000

    Modify sample to build with libopencm3
    Co-authored-by: Marius Brehler <marius.brehler@iml.fraunhofer.de>

--- samples/simple_embedding.c
+++ samples/simple_embedding.c
@@ -8,7 +8,12 @@
 // multiplication with the device implemented by different backends via
 // create_sample_driver().
 
+#include <errno.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/usart.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include "iree/base/api.h"
 #include "iree/hal/api.h"
@@ -157,13 +162,75 @@ iree_status_t Run() {
   return iree_ok_status();
 }
 
-int main() {
+static void clock_setup(void) {
+  /* Enable GPIOD clock for LED & USARTs. */
+  rcc_periph_clock_enable(RCC_GPIOD);
+  rcc_periph_clock_enable(RCC_GPIOA);
+
+  /* Enable clocks for USART2. */
+  rcc_periph_clock_enable(RCC_USART2);
+}
+
+static void usart_setup(void) {
+  /* Setup USART2 parameters. */
+  usart_set_baudrate(USART2, 115200);
+  usart_set_databits(USART2, 8);
+  usart_set_stopbits(USART2, USART_STOPBITS_1);
+  usart_set_mode(USART2, USART_MODE_TX);
+  usart_set_parity(USART2, USART_PARITY_NONE);
+  usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
+
+  /* Finally enable the USART. */
+  usart_enable(USART2);
+}
+
+static void gpio_setup(void) {
+  /* Setup GPIO pin GPIO12 on GPIO port D for LED. */
+  gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
+
+  /* Setup GPIO pins for USART2 transmit. */
+  gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2);
+
+  /* Setup USART2 TX pin as alternate function. */
+  gpio_set_af(GPIOA, GPIO_AF7, GPIO2);
+}
+
+int _write(int file, char* ptr, int len) {
+  int i;
+
+  if (file == STDOUT_FILENO || file == STDERR_FILENO) {
+    for (i = 0; i < len; i++) {
+      if (ptr[i] == '\n') {
+        usart_send_blocking(USART2, '\r');
+      }
+      usart_send_blocking(USART2, ptr[i]);
+    }
+    return i;
+  }
+  errno = EIO;
+  return -1;
+}
+
+int main(void) {
+  clock_setup();
+  gpio_setup();
+  usart_setup();
+
+  printf("Running simple_embedding...\n");
+
   const iree_status_t result = Run();
   int ret = (int)iree_status_code(result);
+
   if (!iree_status_is_ok(result)) {
     iree_status_fprint(stderr, result);
     iree_status_free(result);
+  } else {
+    printf("Execution succesfull!\n");
   }
-  fprintf(stdout, "simple_embedding done\n");
-  return ret;
+  printf("simple_embedding done\n");
+
+  while (1) {
+  }
+
+  return 0;
 }
