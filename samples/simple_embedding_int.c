5ab69aa9fe2ab936478578d877567a32deeccfba
Lucas Camphausen <lucas.camphausen@iml.fraunhofer.de>
2021-12-22 09:41:55 +0100

    Implement clock, gpio and uart setup for crt0.s (#52)
    The implementations are based on those for CMSIS.

--- samples/simple_embedding_int.c
+++ samples/simple_embedding_int.c
@@ -9,16 +9,37 @@
 // create_sample_driver().
 
 #include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+
 #ifdef BUILD_WITH_CMSIS
 #include <stm32f4xx.h>
+#endif
+#ifdef BUILD_WITH_CRT0
+uint32_t SystemCoreClock = 16000000;
+
+const uint32_t GPIOA_BASE = 0x40020000;
+const uint32_t RCC_BASE = 0x40023800;
+const uint32_t USART2_BASE = 0x40004400;
+
+uint32_t* GPIOA_MODER = (uint32_t*)(GPIOA_BASE + 0x00);
+uint32_t* GPIOA_OSPEEDR = (uint32_t*)(GPIOA_BASE + 0x08);
+uint32_t* GPIOA_AFR[2] = {(uint32_t*)(GPIOA_BASE + 0x20),
+                          (uint32_t*)(GPIOA_BASE + 0x24)};
+
+uint32_t* RCC_AHB1ENR = (uint32_t*)(RCC_BASE + 0x30);
+uint32_t* RCC_APB1ENR = (uint32_t*)(RCC_BASE + 0x40);
+
+uint32_t* USART2_SR = (uint32_t*)(USART2_BASE + 0x00);
+uint32_t* USART2_DR = (uint32_t*)(USART2_BASE + 0x04);
+uint32_t* USART2_BRR = (uint32_t*)(USART2_BASE + 0x08);
+uint32_t* USART2_CR1 = (uint32_t*)(USART2_BASE + 0x0C);
 #endif
 #ifdef BUILD_WITH_LIBOPENCM3
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/usart.h>
 #endif
-#include <stdio.h>
-#include <unistd.h>
 
 #include "iree/base/api.h"
 #include "iree/hal/api.h"
@@ -224,16 +245,44 @@ void usart2_send_blocking(uint8_t c) {
 #endif
 
 #ifdef BUILD_WITH_CRT0
-static void clock_setup(void) { /* Not implemented */
+static void clock_setup(void) {
+  /* Enable clock for GPIOA */
+  *RCC_AHB1ENR |= (1 << 0);
+
+  /* Enable clock for USART2 */
+  *RCC_APB1ENR |= (1 << 17);
 }
 
-static void gpio_setup(void) { /* Not implemented */
+static void gpio_setup(void) {
+  /* Configure alternate function mode for GPIOA */
+  *GPIOA_MODER |= (2 << 4);
+
+  /* Configure speed for GPIOA on pin 2 */
+  *GPIOA_OSPEEDR |= (3 << 4);
+
+  /* Configure alternate function mode for GPIOA on pin 2 */
+  *GPIOA_AFR[0] |= (7 << 8);
 }
 
-static void usart_setup(void) { /* Not implemented */
+static void usart_setup(void) {
+  /* Enable USART2 */
+  *USART2_CR1 |= (1 << 13);
+
+  /* Configure word length to 8bit for USART2 */
+  *USART2_CR1 &= ~(1 << 12);
+
+  /* Configure baud rate 115200 for USART2 */
+  /* Computation taken over from libopencm3 */
+  *USART2_BRR = (SystemCoreClock + 115200 / 2) / 115200;
+
+  /* Enable transmitting for USART2 */
+  *USART2_CR1 |= (1 << 3);
 }
 
-void usart2_send_blocking(uint8_t c) { /* Not implemented */
+void usart2_send_blocking(uint8_t c) {
+  *USART2_DR = c;
+  while (!(*USART2_SR & (1 << 6)))
+    ;
 }
 #endif
 
