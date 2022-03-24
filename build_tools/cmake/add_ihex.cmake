2bf7e4318381e8b0bc54ed3853a2efb32605d10d
Marius Brehler <marius.brehler@iml.fraunhofer.de>
2022-03-24 15:20:03 +0100

    Generate ihex files by default
    Adds a CMake function that utilizes objcopy to generate ihex files
    alongside with the ELF files.

---
+++ build_tools/cmake/add_ihex.cmake
@@ -0,0 +1,20 @@
+# Copyright 2022 Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+# add_bare_metal_executable()
+#
+# CMake function to add a hex file.
+
+function(add_ihex TARGET)
+  if(NOT BUILD_IHEX)
+    return()
+  endif()
+
+  add_custom_command(
+    TARGET ${TARGET} POST_BUILD
+    COMMAND ${CMAKE_OBJCOPY} -O ihex ${TARGET} ${TARGET}.hex
+  )
+endfunction()
