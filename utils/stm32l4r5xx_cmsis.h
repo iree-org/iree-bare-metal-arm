c0e3c495f988adeb87cb09338148c688e3a51fee
Lucas Camphausen <lucas.camphausen@iml.fraunhofer.de>
2022-10-26 11:45:34 +0200

    Add `PLL_HSI` clock configuration parameters for STM32L4R5 (#221)
    This adds the PLL parameters for clock source `PLL_HSI`.

--- utils/stm32l4r5xx_cmsis.h
+++ utils/stm32l4r5xx_cmsis.h
@@ -8,3 +8,16 @@
 #include "stm32l4r5xx.h"
 
 #define USART_ISR_TXE USART_ISR_TXE_TXFNF
+
+// When using the PLL, the clock needs to be configured accordingly. Adjust
+// these values to your needs.
+
+#define FLASH_WAIT_STATES FLASH_ACR_LATENCY_3WS
+#define AHB_PRESCALER RCC_CFGR_HPRE_DIV1
+#define APB1_PRESCALER RCC_CFGR_PPRE1_DIV1
+#define APB2_PRESCALER RCC_CFGR_PPRE2_DIV1
+#if defined(USE_PLL_HSI_CLOCK)
+#define PLL_M 0  // divides by 1
+#define PLL_N 10
+#define PLL_R 0  // divides by 2
+#endif
