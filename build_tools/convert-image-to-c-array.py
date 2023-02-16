f7c7364310a67aa909bbf87d17ae87817a8f949a
Simon Camphausen <simon.camphausen@iml.fraunhofer.de>
2023-02-16 11:32:24 +0100

    Add mobilenet v1 targets to samples

--- build_tools/convert-image-to-c-array.py
+++ build_tools/convert-image-to-c-array.py
@@ -17,16 +17,34 @@ import numpy as np
 from PIL import Image
 
 
-def serialize(input_path: str, data_format: str):
+def serialize(input_path: str, data_format: str, resize: str):
     image = Image.open(input_path)
+    if not resize == "none":
+        w, h = resize.split("x")
+        w, h = int(w), int(h)
+        image = image.resize((w, h))
     if data_format == "uint8":
         data = np.array(image).astype(np.uint8)
         value_literal = lambda e: str(e)
         c_type = "uint8_t"
-    else:
-        data = np.array(image).astype(np.float32) / 255.0
+    elif data_format in ("float_0_1", "float_1_1"):
+        data = np.array(image).astype(np.float32)
+
+        if data_format == "float_0_1":
+            low = 0.0
+            high = 1.0
+        elif data_format == "float_1_1":
+            low = -1.0
+            high = 1.0
+        else:
+            raise ValueError("Unsupported data_format")
+        
+        data = (high - low) * data / 255.0 + low
         value_literal = lambda e: f"{e}f"
         c_type = "float"
+    else:
+        raise ValueError("Unsupported data_format")
+
     c_array = f"{{{', '.join(map(value_literal, data.flat))}}}"
     print(f"const {c_type} image[] = {c_array};")
 
@@ -35,11 +53,12 @@ def main():
     parser = argparse.ArgumentParser(
         description="Load an image file and serialize it to a C array"
     )
-    parser.add_argument("--data-format", choices=["uint8", "float"], default="float")
+    parser.add_argument("--data-format", choices=["uint8", "float_0_1", "float_1_1"], default="float_0_1")
+    parser.add_argument("--resize", default="none")
     parser.add_argument("input", help="Path to input image")
     args = parser.parse_args()
 
-    serialize(args.input, args.data_format)
+    serialize(args.input, args.data_format, args.resize)
 
 
 if __name__ == "__main__":
