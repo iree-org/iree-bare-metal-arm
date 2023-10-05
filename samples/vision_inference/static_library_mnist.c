7354815aecbbf948660a8123cfa74c4aa8f00a19
Marius Brehler <marius.brehler@iml.fraunhofer.de>
2023-10-05 13:05:38 +0200

    Integrate IREE at openxla/iree@bb51f6f
    Updates IREE usage to match openxla/iree@bb51f6f and
    candidate-20231004.665, respectively.
    
    This furthermore adjust `C_FLAGS` to not fail on warnings.

--- samples/vision_inference/static_library_mnist.c
+++ samples/vision_inference/static_library_mnist.c
@@ -130,8 +130,8 @@ iree_status_t Run() {
   iree_hal_buffer_view_t* arg0_buffer_view = NULL;
 
   if (iree_status_is_ok(status)) {
-    status = iree_hal_buffer_view_allocate_buffer(
-        iree_hal_device_allocator(device), IREE_ARRAYSIZE(shape), shape,
+    status = iree_hal_buffer_view_allocate_buffer_copy(
+        device, iree_hal_device_allocator(device), IREE_ARRAYSIZE(shape), shape,
         IREE_HAL_ELEMENT_TYPE_FLOAT_32, IREE_HAL_ENCODING_TYPE_DENSE_ROW_MAJOR,
         (iree_hal_buffer_params_t){
             .type = IREE_HAL_MEMORY_TYPE_DEVICE_LOCAL,
