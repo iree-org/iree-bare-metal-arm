0636c4e03adb7e79ffd6985885e1c1fd0dd57730
Lucas Camphausen <lucas.camphausen@iml.fraunhofer.de>
2023-11-06 15:57:10 +0100

    Fix UARTE implementation for nRF5340
    Co-authored-by: Marius Brehler <marius.brehler@iml.fraunhofer.de>

--- utils/nrf5340_cmsis.c
+++ utils/nrf5340_cmsis.c
@@ -17,15 +17,14 @@ nrfx_uarte_t uarte = NRFX_UARTE_INSTANCE(0);
 
 void clock_setup(void) {}
 
-// TODO: Fix me!
 void usart_setup(void) {
 #ifdef USE_UART0
   // Configuration for UARTE
   const nrfx_uarte_config_t uarte_config = {
-      .pseltxd = NRF_GPIO_PIN_MAP(0, 6),
-      .pselrxd = NRF_GPIO_PIN_MAP(0, 8),
-      .pselcts = NRF_UARTE_PSEL_DISCONNECTED,
-      .pselrts = NRF_UARTE_PSEL_DISCONNECTED,
+      .txd_pin = NRF_GPIO_PIN_MAP(1, 1),
+      .rxd_pin = NRF_GPIO_PIN_MAP(1, 0),
+      .rts_pin = NRF_UARTE_PSEL_DISCONNECTED,
+      .cts_pin = NRF_UARTE_PSEL_DISCONNECTED,
       .baudrate = UARTE_BAUDRATE_BAUDRATE_Baud115200,
   };
 
@@ -38,7 +37,7 @@ void gpio_setup(void) {}
 
 void send_blocking(uint16_t c) {
 #ifdef USE_UART0
-  nrfx_uarte_tx(&uarte, (uint8_t*)&c, 1);
+  nrfx_uarte_tx(&uarte, (uint8_t*)&c, 1, NRFX_UARTE_TX_BLOCKING);
 #endif
 }
 
